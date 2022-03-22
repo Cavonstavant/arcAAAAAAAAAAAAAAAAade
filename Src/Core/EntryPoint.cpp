ceb090b983a9434c53071d4c76f3200c9d06ecac
aurelenc <aurele.nicolas@epitech.eu>
2022-03-22 19:55:26 +0100

    Removed testing code in the main function

--- Src/Core/EntryPoint.cpp
+++ Src/Core/EntryPoint.cpp
@@ -10,29 +10,5 @@
 
 int main(void)
 {
-    try {
-        throw ArcadeEX("Error");
-    } catch (ArcadeException &ex) {
-        std::cout << "EXCEPTION " << ex.getName() << std::endl;
-        std::cout << "WHAT : " << ex.what() << std::endl;
-        std::cout << "WHERE: " << ex.where() << std::endl;
-    }
-    std::cout << std::endl;
-    try {
-        throw FactoryEX("Error");
-    } catch (ArcadeException &ex) {
-        std::cout << "EXCEPTION " << ex.getName() << std::endl;
-        std::cout << "WHAT : " << ex.what() << std::endl;
-        std::cout << "WHERE: " << ex.where() << std::endl;
-    }
-    std::cout << std::endl;
-    try {
-        throw VeryStupidUserEX("");
-    } catch (ArcadeException &ex) {
-        std::cout << "EXCEPTION " << ex.getName() << std::endl;
-        std::cout << "WHAT : " << ex.what() << std::endl;
-        std::cout << "WHERE: " << ex.where() << std::endl;
-    }
-    std::cout << std::endl;
     return (0);
 }
