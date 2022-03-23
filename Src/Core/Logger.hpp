ca7e2002605bf091191a21bc2861171c07e0bba0
Restyled.io <commits@restyled.io>
2022-03-23 01:37:21 +0000

    Restyled by astyle

--- Src/Core/Logger.hpp
+++ Src/Core/Logger.hpp
@@ -11,20 +11,20 @@
 class ArcadeException;
 
 class Logger {
-    public:
-        /// \brief log the exception before throwing it
-        /// \param ex the exception to log
-        /// \return the exception
-        static ArcadeException log(ArcadeException exception);
-        /// \brief pass this function to the macros and they will log the exception
-        /// \return true
-        static bool ENABLED();
-        /// \brief pass this function to the macros and they will not log the exception
-        /// \return false
-        static bool DISABLED();
+public:
+    /// \brief log the exception before throwing it
+    /// \param ex the exception to log
+    /// \return the exception
+    static ArcadeException log(ArcadeException exception);
+    /// \brief pass this function to the macros and they will log the exception
+    /// \return true
+    static bool ENABLED();
+    /// \brief pass this function to the macros and they will not log the exception
+    /// \return false
+    static bool DISABLED();
 
-    protected:
-    private:
+protected:
+private:
 };
 
 #endif /* !LOGGER_HPP_ */
