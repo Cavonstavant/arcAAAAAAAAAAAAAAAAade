9c3796af9983aec859370ebaafdd7ea20fc8e5ef
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-03-24 17:33:12 +0100

    [+] Add Player.hpp & Player.cpp

---
+++ Src/Core/Entity/Player.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2022
+** Player.cpp.cc
+** File description:
+** Created by hjulien,
+*/
+
+#include "Player.hpp"
+
+Player::Player() : AEntity()
+{
+    _speed = 0;
+}
+
+Player::~Player()
+{
+}
+
+void Player::setIsMoving(bool isMoving)
+{
+    _isMoving = isMoving;
+}
+
+bool Player::getIsMoving() const
+{
+    return _isMoving;
+}
+
+void Player::setBoosted(const bool boosted)
+{
+    _boosted = boosted;
+}
+
+bool Player::getBoosted() const
+{
+    return _boosted;
+}
