2ed39b2b8d3b6fcf3c640e920d47f239c50508ec
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-04-03 00:54:10 +0200

    [*] Test compile with bot dependencies

--- Src/Core/EntryPoint.cpp
+++ Src/Core/EntryPoint.cpp
@@ -7,14 +7,27 @@
 
 #include "Core.hpp"
 #include "../GraphicLibs/SDL/SdlLibrary.hpp"
+#include "../GraphicLibs/SFML/SfmlLibrary.hpp"
 
 int main(int ac, char **av)
 {
     (void)ac;
     (void)av;
-    SdlLibrary library;
+    /*SdlLibrary library;
 
     library.displayWindow();
+    library.clearWindow();*/
+
+    SfmlLibrary library;
+    Color color(0, 0, 0, 255);
+    std::pair<int, int> pos;
+    pos.first = 100;
+    pos.second = 100;
+
+    while (true) {
+        library.displayWindow();
+        library.drawRect(pos, 100, 100, color);
+    }
     library.clearWindow();
     return (0);
 }
