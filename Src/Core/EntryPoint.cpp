dd6dc0562b03333ebdb1f83896726300ae2b4721
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-04-02 23:28:58 +0200

    [*] Updating Cmakelist.txt (need to fix) | EntryPoint.cpp (need to work)

--- Src/Core/EntryPoint.cpp
+++ Src/Core/EntryPoint.cpp
@@ -6,8 +6,15 @@
 */
 
 #include "Core.hpp"
+#include "../GraphicLibs/SDL/SdlLibrary.hpp"
 
 int main(int ac, char **av)
 {
+    (void)ac;
+    (void)av;
+    SdlLibrary library;
+
+    library.displayWindow();
+    library.clearWindow();
     return (0);
 }
