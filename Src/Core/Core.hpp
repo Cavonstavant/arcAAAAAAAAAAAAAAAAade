b06af135fced5edb1cb9d0cd936a0e176c80bc7b
Cavonstavant <constant.vigneron@protonmail.com>
2022-04-02 23:26:55 +0200

    [+] Add basic Core class

--- Src/Core/Core.hpp
+++ Src/Core/Core.hpp
@@ -8,6 +8,25 @@
 #ifndef ARCAAAAAAAAAAAAAAAAADE_CORE_HPP
 #define ARCAAAAAAAAAAAAAAAAADE_CORE_HPP
 
+#include "IEntity.hpp"
+#include "IGame.hpp"
+#include "IGraph.hpp"
+#include <vector>
 
+class Core {
+    public:
+        Core();
+        ~Core();
+        void addEntity(IEntity *entity);
+        void removeEntity(IEntity *entity);
+        void update();
+        void draw();
+    private:
+        std::vector<IEntity *> _entities;
+        IGame *_game;
+        template<typename T>
+        IGraph<T>* _graph;
+
+}
 
 #endif//ARCAAAAAAAAAAAAAAAAADE_CORE_HPP
