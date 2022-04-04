ff9317f687d6e229785bff09164cd8ebdf676c60
Restyled.io <commits@restyled.io>
2022-04-04 23:44:47 +0000

    Restyled by astyle

--- Src/Games/Common/AEntity.hpp
+++ Src/Games/Common/AEntity.hpp
@@ -13,49 +13,49 @@
 
 /// \brief Entity Abstract class
 class AEntity : public IEntity {
-    public:
-        /// \brief Default Constructor
-        AEntity();
-        /// \brief Destructor
-        ~AEntity() = default;
+public:
+    /// \brief Default Constructor
+    AEntity();
+    /// \brief Destructor
+    ~AEntity() = default;
 
-        /// \brief Get the entity position
-        /// \return The std::pair<int, int> position
-        std::pair<int, int> getPos() const override;
+    /// \brief Get the entity position
+    /// \return The std::pair<int, int> position
+    std::pair<int, int> getPos() const override;
 
-        /// \brief Set the entity position
-        /// \param Pos the std::pair<int, int> position
-        void setPos(std::pair<int, int> pos) override;
+    /// \brief Set the entity position
+    /// \param Pos the std::pair<int, int> position
+    void setPos(std::pair<int, int> pos) override;
 
-        /// \brief Get the entity size
-        /// \return The std::pair<int, int> size
-        std::pair<int, int> getSize() const override;
+    /// \brief Get the entity size
+    /// \return The std::pair<int, int> size
+    std::pair<int, int> getSize() const override;
 
-        /// \brief Set the entity size
-        /// \param Size the std::pair<int, int> size
-        void setSize(std::pair<int, int> size) override;
+    /// \brief Set the entity size
+    /// \param Size the std::pair<int, int> size
+    void setSize(std::pair<int, int> size) override;
 
-        /// \brief Set the Entity boolean isMoving
-        virtual void setIsMoving(bool isMoving);
+    /// \brief Set the Entity boolean isMoving
+    virtual void setIsMoving(bool isMoving);
 
-        /// \brief Get the Entity boolean isMoving
-        /// \return bool isMoving;
-        virtual bool getIsMoving() const;
+    /// \brief Get the Entity boolean isMoving
+    /// \return bool isMoving;
+    virtual bool getIsMoving() const;
 
-        /// \brief Set the texturePath of the entity
-        /// \param std::string texturePath represents the path to find the texture
-        void setTexturePath(std::string texturePath) override;
+    /// \brief Set the texturePath of the entity
+    /// \param std::string texturePath represents the path to find the texture
+    void setTexturePath(std::string texturePath) override;
 
-        /// \brief Get the texturePAth of the Entity
-        std::string getTexturePath(void) const override;
+    /// \brief Get the texturePAth of the Entity
+    std::string getTexturePath(void) const override;
 
-    protected:
-        std::pair<int, int> _pos;
-        std::pair<int, int> _size;
-        bool _isMoving;
-        std::string _texturePath;
+protected:
+    std::pair<int, int> _pos;
+    std::pair<int, int> _size;
+    bool _isMoving;
+    std::string _texturePath;
 
-    private:
+private:
 };
 
 #endif /* !AENTITY_HPP_ */
--- Src/Games/MainMenu/MainMenu.hpp
+++ Src/Games/MainMenu/MainMenu.hpp
@@ -12,37 +12,37 @@
 #include "IGraph.hpp"
 
 class MainMenu : public IGame {
-    public:
-        /// \brief Constructor
-        MainMenu(std::vector<std::shared_ptr<IEntity>> &entities);
-        /// \brief Destructor
-        ~MainMenu();
+public:
+    /// \brief Constructor
+    MainMenu(std::vector<std::shared_ptr<IEntity>> &entities);
+    /// \brief Destructor
+    ~MainMenu();
 
-        /// \brief Update the game's entities
-        /// \param entities The entity vector of the game
-        /// \param events The event stack of the game
-        void update(std::vector<std::shared_ptr<IEntity>> &entities, std::stack<Arcade::Evt> &events) override;
-        /// \brief Get the game's status
-        /// \return the game's status
-        GameState getState() const;
-        /// \brief starts the game
-        void start();
+    /// \brief Update the game's entities
+    /// \param entities The entity vector of the game
+    /// \param events The event stack of the game
+    void update(std::vector<std::shared_ptr<IEntity>> &entities, std::stack<Arcade::Evt> &events) override;
+    /// \brief Get the game's status
+    /// \return the game's status
+    GameState getState() const;
+    /// \brief starts the game
+    void start();
 
-    protected:
-    private:
-        std::vector<std::shared_ptr<Button>> _buttons;
-        GameState _gameState;
+protected:
+private:
+    std::vector<std::shared_ptr<Button>> _buttons;
+    GameState _gameState;
 
-        /// \brief Manage the events
-        /// \param events The event stack of the game
-        /// \param entities The entity vector of the game
-        void manageEvent(Arcade::Evt &event, std::vector<std::shared_ptr<IEntity>> &entities);
-        /// \brief Manage the click events
-        /// \param event The event stack of the game
-        /// \param entities The entity vector of the game
-        void manageClickEvent(Arcade::Evt &event, std::vector<std::shared_ptr<IEntity>> &entities);
-        /// \brief Manage the key events
-        /// \param event The event stack of the game
-        /// \param entities The entity vector of the game
-        void manageKeyEvent(Arcade::Evt &event, std::vector<std::shared_ptr<IEntity>> &entities);
+    /// \brief Manage the events
+    /// \param events The event stack of the game
+    /// \param entities The entity vector of the game
+    void manageEvent(Arcade::Evt &event, std::vector<std::shared_ptr<IEntity>> &entities);
+    /// \brief Manage the click events
+    /// \param event The event stack of the game
+    /// \param entities The entity vector of the game
+    void manageClickEvent(Arcade::Evt &event, std::vector<std::shared_ptr<IEntity>> &entities);
+    /// \brief Manage the key events
+    /// \param event The event stack of the game
+    /// \param entities The entity vector of the game
+    void manageKeyEvent(Arcade::Evt &event, std::vector<std::shared_ptr<IEntity>> &entities);
 };
