6561a635eda706ec9b783209960f6915ef05f7a2
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-04-04 17:10:49 +0200

    [*] clean entrypoint.cpp

--- Src/Core/EntryPoint.cpp
+++ Src/Core/EntryPoint.cpp
@@ -13,21 +13,5 @@ int main(int ac, char **av)
 {
     (void)ac;
     (void)av;
-    /*SdlLibrary library;
-
-    library.displayWindow();
-    library.clearWindow();*/
-
-    SfmlLibrary library;
-    Color color(0, 0, 0, 255);
-    std::pair<int, int> pos;
-    pos.first = 100;
-    pos.second = 100;
-
-    while (true) {
-        library.displayWindow();
-        library.drawRect(pos, 100, 100, color);
-    }
-    library.clearWindow();
     return (0);
 }
