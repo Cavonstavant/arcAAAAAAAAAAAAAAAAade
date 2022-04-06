3b5fe15691777a0e1b169c7ea60215abec9b3e1a
Restyled.io <commits@restyled.io>
2022-04-06 20:46:59 +0000

    Restyled by astyle

--- Src/GraphicLibs/Ncurses/NcursesLibrary.cpp
+++ Src/GraphicLibs/Ncurses/NcursesLibrary.cpp
@@ -94,170 +94,170 @@ Arcade::Evt NcursesLibrary::getInput() const
     int ch = getch();
 
     switch (ch) {
-            case KEY_LEFT:
-                keyEvt.key = 'Q';
-                evt.key = keyEvt;
-                break;
-            case KEY_RIGHT:
-                keyEvt.key = 'D';
-                evt.key = keyEvt;
-                break;
-            case KEY_DOWN:
-                keyEvt.key = 'S';
-                evt.key = keyEvt;
-                break;
-            case KEY_UP:
-                keyEvt.key = 'Z';
-                evt.key = keyEvt;
-                break;
-            case ' ':
-                keyEvt.key = ' ';
-                evt.key = keyEvt;
-                break;
-            case KEY_BACKSPACE:
-                keyEvt.key = '\b';
-                evt.key = keyEvt;
-                break;
-            case KEY_ENTER:
-                keyEvt.key = '\n';
-                evt.key = keyEvt;
-                break;
-            case 27:
-                keyEvt.key = 27;
-                evt.key = keyEvt;
-                break;
-            case 'A':
-            case 'a':
-                keyEvt.key = 'a';
-                evt.key = keyEvt;
-                break;
-            case 'B':
-            case 'b':
-                keyEvt.key = 'b';
-                evt.key = keyEvt;
-                break;
-            case 'C':
-            case 'c':
-                keyEvt.key = 'c';
-                evt.key = keyEvt;
-                break;
-            case 'D':
-            case 'd':
-                keyEvt.key = 'd';
-                evt.key = keyEvt;
-                break;
-            case 'E':
-            case 'e':
-                keyEvt.key = 'e';
-                evt.key = keyEvt;
-                break;
-            case 'F':
-            case 'f':
-                keyEvt.key = 'f';
-                evt.key = keyEvt;
-                break;
-            case 'G':
-            case 'g':
-                keyEvt.key = 'g';
-                evt.key = keyEvt;
-                break;
-            case 'H':
-            case 'h':
-                keyEvt.key = 'h';
-                evt.key = keyEvt;
-                break;
-            case 'I':
-            case 'i':
-                keyEvt.key = 'i';
-                evt.key = keyEvt;
-                break;
-            case 'J':
-            case 'j':
-                keyEvt.key = 'j';
-                evt.key = keyEvt;
-                break;
-            case 'K':
-            case 'k':
-                keyEvt.key = 'k';
-                evt.key = keyEvt;
-                break;
-            case 'L':
-            case 'l':
-                keyEvt.key = 'l';
-                evt.key = keyEvt;
-                break;
-            case 'M':
-            case 'm':
-                keyEvt.key = 'm';
-                evt.key = keyEvt;
-                break;
-            case 'N':
-            case 'n':
-                keyEvt.key = 'N';
-                evt.key = keyEvt;
-                break;
-            case 'O':
-            case 'o':
-                keyEvt.key = 'o';
-                evt.key = keyEvt;
-                break;
-            case 'P':
-            case 'p':
-                keyEvt.key = 'p';
-                evt.key = keyEvt;
-                break;
-            case 'Q':
-            case 'q':
-                keyEvt.key = 'q';
-                evt.key = keyEvt;
-                break;
-            case 'R':
-            case 'r':
-                keyEvt.key = 'r';
-                evt.key = keyEvt;
-                break;
-            case 'S':
-            case 's':
-                keyEvt.key = 's';
-                evt.key = keyEvt;
-                break;
-            case 'T':
-            case 't':
-                keyEvt.key = 't';
-                evt.key = keyEvt;
-                break;
-            case 'U':
-            case 'u':
-                keyEvt.key = 'u';
-                evt.key = keyEvt;
-                break;
-            case 'V':
-            case 'v':
-                keyEvt.key = 'v';
-                evt.key = keyEvt;
-                break;
-            case 'W':
-            case 'w':
-                keyEvt.key = 'w';
-                evt.key = keyEvt;
-                break;
-            case 'X':
-            case 'x':
-                keyEvt.key = 'x';
-                evt.key = keyEvt;
-                break;
-            case 'Y':
-            case 'y':
-                keyEvt.key = 'y';
-                evt.key = keyEvt;
-                break;
-            case 'Z':
-            case 'z':
-                keyEvt.key = 'z';
-                evt.key = keyEvt;
-                break;
-            default:
-                break;
+    case KEY_LEFT:
+        keyEvt.key = 'Q';
+        evt.key = keyEvt;
+        break;
+    case KEY_RIGHT:
+        keyEvt.key = 'D';
+        evt.key = keyEvt;
+        break;
+    case KEY_DOWN:
+        keyEvt.key = 'S';
+        evt.key = keyEvt;
+        break;
+    case KEY_UP:
+        keyEvt.key = 'Z';
+        evt.key = keyEvt;
+        break;
+    case ' ':
+        keyEvt.key = ' ';
+        evt.key = keyEvt;
+        break;
+    case KEY_BACKSPACE:
+        keyEvt.key = '\b';
+        evt.key = keyEvt;
+        break;
+    case KEY_ENTER:
+        keyEvt.key = '\n';
+        evt.key = keyEvt;
+        break;
+    case 27:
+        keyEvt.key = 27;
+        evt.key = keyEvt;
+        break;
+    case 'A':
+    case 'a':
+        keyEvt.key = 'a';
+        evt.key = keyEvt;
+        break;
+    case 'B':
+    case 'b':
+        keyEvt.key = 'b';
+        evt.key = keyEvt;
+        break;
+    case 'C':
+    case 'c':
+        keyEvt.key = 'c';
+        evt.key = keyEvt;
+        break;
+    case 'D':
+    case 'd':
+        keyEvt.key = 'd';
+        evt.key = keyEvt;
+        break;
+    case 'E':
+    case 'e':
+        keyEvt.key = 'e';
+        evt.key = keyEvt;
+        break;
+    case 'F':
+    case 'f':
+        keyEvt.key = 'f';
+        evt.key = keyEvt;
+        break;
+    case 'G':
+    case 'g':
+        keyEvt.key = 'g';
+        evt.key = keyEvt;
+        break;
+    case 'H':
+    case 'h':
+        keyEvt.key = 'h';
+        evt.key = keyEvt;
+        break;
+    case 'I':
+    case 'i':
+        keyEvt.key = 'i';
+        evt.key = keyEvt;
+        break;
+    case 'J':
+    case 'j':
+        keyEvt.key = 'j';
+        evt.key = keyEvt;
+        break;
+    case 'K':
+    case 'k':
+        keyEvt.key = 'k';
+        evt.key = keyEvt;
+        break;
+    case 'L':
+    case 'l':
+        keyEvt.key = 'l';
+        evt.key = keyEvt;
+        break;
+    case 'M':
+    case 'm':
+        keyEvt.key = 'm';
+        evt.key = keyEvt;
+        break;
+    case 'N':
+    case 'n':
+        keyEvt.key = 'N';
+        evt.key = keyEvt;
+        break;
+    case 'O':
+    case 'o':
+        keyEvt.key = 'o';
+        evt.key = keyEvt;
+        break;
+    case 'P':
+    case 'p':
+        keyEvt.key = 'p';
+        evt.key = keyEvt;
+        break;
+    case 'Q':
+    case 'q':
+        keyEvt.key = 'q';
+        evt.key = keyEvt;
+        break;
+    case 'R':
+    case 'r':
+        keyEvt.key = 'r';
+        evt.key = keyEvt;
+        break;
+    case 'S':
+    case 's':
+        keyEvt.key = 's';
+        evt.key = keyEvt;
+        break;
+    case 'T':
+    case 't':
+        keyEvt.key = 't';
+        evt.key = keyEvt;
+        break;
+    case 'U':
+    case 'u':
+        keyEvt.key = 'u';
+        evt.key = keyEvt;
+        break;
+    case 'V':
+    case 'v':
+        keyEvt.key = 'v';
+        evt.key = keyEvt;
+        break;
+    case 'W':
+    case 'w':
+        keyEvt.key = 'w';
+        evt.key = keyEvt;
+        break;
+    case 'X':
+    case 'x':
+        keyEvt.key = 'x';
+        evt.key = keyEvt;
+        break;
+    case 'Y':
+    case 'y':
+        keyEvt.key = 'y';
+        evt.key = keyEvt;
+        break;
+    case 'Z':
+    case 'z':
+        keyEvt.key = 'z';
+        evt.key = keyEvt;
+        break;
+    default:
+        break;
     }
     return evt;
 }
