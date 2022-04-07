f607ee49b03f0ed0606e26a9437ad5cec3de3fa6
Cavonstavant <constant.vigneron@protonmail.com>
2022-04-07 02:42:26 +0200

    [*] Added Core class and basic methods and implementations

--- Src/Core/Core.cpp
+++ Src/Core/Core.cpp
@@ -5,7 +5,21 @@
 ** core
 */
 
+#include <vector>
+#include <string>
 #include "Core.hpp"
+#include <iostream>
+
+Core::Core(std::vector<std::string> libsPath)
+{
+    std::vector<std::shared_ptr<IEntity>> _ett; //< Test vector of entities
+    _mainMenu.init(_ett);
+    std::cout << "Lib size: " << libsPath.size() << std::endl;
+    _libManager.addLib(libsPath);
+    _game = &_mainMenu;
+    _graph = _libManager.openGraph(libsPath.begin()->c_str());
+    _mainMenu.start();
+}
 
 void Core::addEntity(std::shared_ptr<IEntity> entity)
 {
@@ -15,9 +29,4 @@ void Core::addEntity(std::shared_ptr<IEntity> entity)
 void Core::removeEntity(std::shared_ptr<IEntity> entity)
 {
     _entities.erase(std::remove(_entities.begin(), _entities.end(), entity), _entities.end());
-}
-
-std::vector<std::shared_ptr<IEntity>>& Core::getEntities()
-{
-    return _entities;
 }
--- Src/Core/Core.hpp
+++ Src/Core/Core.hpp
@@ -11,22 +11,56 @@
 #include "IEntity.hpp"
 #include "IGame.hpp"
 #include "IGraph.hpp"
+#include "MainMenu/MainMenu.hpp"
+#include "LibManager.hpp"
 #include <vector>
 
 class Core {
     public:
-        Core() = default;
+        /// \brief Creating a core without at least a path to a library should be impossible
+        Core() = delete;
+
+        /// \brief Creating a core with a variable number of paths to libraries
+        Core(std::vector<std::string> libsPath);
+
+        /// \brief Dtor
         ~Core() = default;
+
+        /// \brief Construct a core with an other core should not be possible
         Core(Core const &) = delete;
+
+        /// \brief Assign a core to another should not be possible
         Core& operator=(Core const &) = delete;
+
+        /// \brief Add an entity to the entities collection
+        /// \param entity The entity to add
         void addEntity(std::shared_ptr<IEntity> entity);
+
+        /// \brief Remove an entity to the entites collection
+        /// \param entity The entity collection to remove
         void removeEntity(std::shared_ptr<IEntity> entity);
-        std::vector<std::shared_ptr<IEntity>>& getEntities();
+
+        /// \brief Update the data of all entities by passing them to the game
         void update();
+
+        /// \brief Update the graphical interface by passing all the entiteis to the graph
         void draw();
     private:
+        /// \brief The library manager containing all the libraries
+        LibManager _libManager;
+
+        /// \brief The entities collection
         std::vector<std::shared_ptr<IEntity>> _entities;
+
+        /// \brief The main menu
+        MainMenu _mainMenu;
+
+        /// \brief The game currently running
         IGame *_game;
+
+        /// \brief The graphical interface currently running
+        /// \note Will be filled with the instance retrieved from the graphical library passed by argument (argv[1])
+        IGraph *_graph;
 };
 
 #endif//ARCAAAAAAAAAAAAAAAAADE_CORE_HPP
