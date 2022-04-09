871400614a91bf8273fcab7f2b49761475991913
Cavonstavant <constant.vigneron@protonmail.com>
2022-04-09 15:19:49 +0200

    [*] Update CMakeLists.txt and force load pacman for tests purposes

--- Src/Core/Core.cpp
+++ Src/Core/Core.cpp
@@ -16,7 +16,8 @@ Core::Core(std::vector<std::string> libsPath)
 {
     _mainMenu.init(_entities);
     _libManager.addLibs(libsPath);
-    _game = &_mainMenu;
+//    _game = &_mainMenu;
+    _game = _libManager.openGame("lib/arcade_pacman.so");
     _graph = _libManager.openGraph(*libsPath.begin());
     _graph->init();
     _mainMenu.start();
