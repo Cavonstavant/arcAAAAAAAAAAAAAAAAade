2402fbc16649f191bf6a1cdce070ff15516107c6
Hourcadette Julien <julien.hourcadette@epitech.eu>
2022-04-10 12:52:41 +0200

    [+] add entryPoint

---
+++ Src/GraphicLibs/SDL/EntryPoint.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "IGraph.hpp"
+#include "SdlLibrary.hpp"
+
+namespace Arcade
+{
+    static IGraph *GRAPH_INSTANCE = nullptr;
+
+    extern "C" IGraph *getGraphInstance()
+    {
+        return GRAPH_INSTANCE;
+    }
+
+    [[gnu::constructor]] void libConstruct()
+    {
+        GRAPH_INSTANCE = new SdlLibrary();
+    }
+
+    [[gnu::destructor]] void libDestruct()
+    {
+        delete GRAPH_INSTANCE;
+    }
+}
